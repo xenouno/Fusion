4d21208e4c164a49e40e936d6ceecdc66ed7e3b4
xenouno <112983459+xenouno@users.noreply.github.com>
2024-03-16 16:05:30 +0530

    Revert "Otheracademic 2024 (#1316)"
    This reverts commit dd35ab61954c0a388d30c40233ad1c48bc510b06.

--- FusionIIIT/Fusion/settings/common.py
+++ FusionIIIT/Fusion/settings/common.py
@@ -127,7 +127,6 @@ INSTALLED_APPS = [
     'applications.ps1',
     'applications.programme_curriculum',
     'applications.placement_cell',
-    #   'applications.otheracademic',
     'applications.recruitment',
     'applications.scholarships',
     'applications.visitor_hostel',
